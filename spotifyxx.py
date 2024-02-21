801e521efd490fd47cd57ae8ada33ae896233e98
kionling <djvdjv209@gmail.com>
2024-02-21 11:25:21 -0800

    formatted song retrival

--- spotifyxx.py
+++ spotifyxx.py
@@ -12,15 +12,7 @@ scope = "user-read-playback-state"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
 result = sp.current_playback(market='US', additional_types=('track'))
-
-# print(result.get('item').get('name'))
-# print(result.get('item').get('name'), '-', result.get('item').get('artists')[0].get('name'))
-
-
-# Iterate through the list of artists and print their names
-for artist in result.get('item').get('artists'):
-    print(artist['name'])
-
-
-# print(json.dumps(result.get('item').get('artists'), sort_keys=True, indent=4))
-
+print(result.get('item').get('album').get('name'))
+artists = [artist['name'] for artist in result.get('item').get('artists')]
+artist_names = ', '.join(artists)
+print(result.get('item').get('name'), '-', artist_names)
