87163a77d79e0213a6aaf8ee7cdeedf81644a3e8
kionling <djvdjv209@gmail.com>
2024-02-21 11:14:43 -0800

    iterated through all artists name if featured artist is there

--- spotifyxx.py
+++ spotifyxx.py
@@ -11,8 +11,16 @@ scope = "user-read-playback-state"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-result = sp.current_playback(market=None, additional_types=('track'))
+result = sp.current_playback(market='US', additional_types=('track'))
 
-print(result.get('item').get('name'), result.get('item').get('artists')[0].get('name'))
+# print(result.get('item').get('name'))
+# print(result.get('item').get('name'), '-', result.get('item').get('artists')[0].get('name'))
 
+
+# Iterate through the list of artists and print their names
+for artist in result.get('item').get('artists'):
+    print(artist['name'])
+
+
+# print(json.dumps(result.get('item').get('artists'), sort_keys=True, indent=4))
 
